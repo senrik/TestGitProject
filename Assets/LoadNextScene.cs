5912d42ff637e37a192741302022003e07e3f439
Erik <erik.swanson@gtri.gatech.edu>
2017-05-02 16:33:08 -0400

    Init Commit

---
+++ Assets/LoadNextScene.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    void LoadScene()
+    {
+        if (SceneManager.GetActiveScene().name == "scene01")
+        {
+            SceneManager.LoadScene("scene02");
+        }
+        else
+        {
+            SceneManager.LoadScene("scene01");
+        }
+    }
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            LoadScene();
+        }
+	}
+}
