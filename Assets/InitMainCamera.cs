21253f80c6aa3d4078f18c114b63e3c0db23e9b0
Erik <erik.swanson@gtri.gatech.edu>
2017-05-02 16:51:26 -0400

    Main Camera instantiation added.

---
+++ Assets/InitMainCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InitMainCamera : MonoBehaviour {
+
+    public GameObject mainCamera;
+
+	// Use this for initialization
+	void Start () {
+        if (!GameObject.FindGameObjectWithTag("MainCamera"))
+        {
+            Instantiate(mainCamera);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
